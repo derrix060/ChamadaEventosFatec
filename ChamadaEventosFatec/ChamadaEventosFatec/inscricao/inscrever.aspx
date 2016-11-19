7e611e8ba7b73a4b51cfe5e93f8bf4dc969b1a38
derrix060 <mario_surfing@hotmail.com>
2016-11-19 11:51:56 -0200

    inscricao

---
+++ ChamadaEventosFatec/ChamadaEventosFatec/inscricao/inscrever.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inscrever.aspx.cs" Inherits="ChamadaEventosFatec.inscricao.inscrever" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <title>Inscricao</title>
+    <!-- Bootstrap -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <!-- Bootstrap Select -->
+    <link rel="stylesheet" type="text/css" media="screen" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/css/bootstrap-select.min.css"/>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/js/bootstrap-select.min.js"></script>
+
+
+</head>
+<body>
+    <form id="formInscricao" runat="server" class="container">
+
+        <div class="jumbotron">
+            <h1>Formulario de inscricao</h1>
+            <h3>Bem-vindo aluno!</h3>
+        </div>
+       
+        
+        <div class="form-group">
+            
+            <!-- Periodo -->
+            <div class="form-group">
+                <asp:Label runat="server" for="dropPeriodo">Periodo</asp:Label><br />
+                <asp:DropDownList runat="server" CssClass="selectpicker" ID="dropPeriodo" AutoPostBack="true" OnSelectedIndexChanged="Refresh_Palestra_Materia"></asp:DropDownList>
+
+                <asp:RequiredFieldValidator ID="rfvPeriodo" runat="server" ControlToValidate="dropPeriodo" ErrorMessage="Campo obrigatório!" CssClass="label label-danger"></asp:RequiredFieldValidator>
+            </div>
+
+
+            <!-- Dia -->
+            <div class="form-group">
+                <asp:Label runat="server">Dias</asp:Label><br />
+                <asp:DropDownList runat="server" CssClass="selectpicker" ID="dropDia" AutoPostBack="true" OnSelectedIndexChanged="Refresh_Palestra_Materia"></asp:DropDownList>
+                    
+                <asp:RequiredFieldValidator ID="rfvDia" runat="server" ControlToValidate="dropDia" ErrorMessage="Campo obrigatório!" CssClass="label label-danger"></asp:RequiredFieldValidator>
+            </div>
+
+            <!-- Palestra -->
+            <div class="form-group">
+                <asp:Label runat="server">Palestras</asp:Label><br />
+                <asp:DropDownList runat="server" CssClass="selectpicker show-tick" data-live-search="true" ID="dropPalestra" multiple="true"></asp:DropDownList>
+                    
+                <asp:RequiredFieldValidator ID="rfvPalestra" runat="server" ControlToValidate="dropPalestra" ErrorMessage="Campo obrigatório!" CssClass="label label-danger"></asp:RequiredFieldValidator>
+            </div>
+        
+        
+            <!-- Materia -->
+            <div class="form-group">
+                <asp:Label runat="server">Materias</asp:Label><br />
+                <asp:DropDownList runat="server" CssClass="selectpicker show-tick" data-live-search="true" ID="dropMateria"  multiple="true"></asp:DropDownList>
+                    
+                <asp:RequiredFieldValidator ID="rfvMateria" runat="server" ControlToValidate="dropMateria" ErrorMessage="Campo obrigatório!" CssClass="label label-danger"></asp:RequiredFieldValidator>
+            </div>
+        </div>
+        
+        
+
+        <!-- Inscricao -->
+        <asp:Button ID="btnInscrever" runat="server" CssClass="btn btn-primary" Text="Submit" />
+
+    </form>
+
+    
+    <script type="text/javascript">
+            $(document).ready(function() {  
+                $(".selectpicker").selectpicker();
+            });
+    </script>
+</body>
+</html>
---
+++ ChamadaEventosFatec/ChamadaEventosFatec/inscricao/inscrever.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//DataSet
+using System.Data;
+
+//Database
+using MySql.Data.MySqlClient;
+
+namespace ChamadaEventosFatec.inscricao
+{
+    public partial class inscrever : System.Web.UI.Page
+    {
+        static string sc = "Server=localhost;Database=chamada;uid=visual_studio;pwd=visual;";
+        static string sql;
+        static MySqlConnection conn = new MySqlConnection(sc);
+
+        static MySqlDataAdapter conversor = new MySqlDataAdapter();
+        static DataSet ds = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!Page.IsPostBack)
+            {
+                //carrega tabelas no Dataset
+                Carrega_Periodo();
+                Carrega_Dia();
+                Carrega_Palestra();
+                Carrega_Materia();
+
+                //load periodo
+                dropPeriodo.DataSource = ds.Tables["tbPeriodo"];
+                dropPeriodo.DataTextField = "cod";
+                dropPeriodo.DataBind();
+
+                //load dias
+                dropDia.DataSource = ds.Tables["tbDia"];
+                dropDia.DataTextField = "dia";
+                dropDia.DataBind();
+            }
+
+        }
+
+        protected void Refresh_Palestra_Materia(object sender, EventArgs e)
+        {
+            string strFilterPeriodo = "";
+            string strFilterDia = "";
+
+
+            //filter Periodo
+            strFilterPeriodo = "periodo='" + dropPeriodo.SelectedValue + "'";
+
+            //filter Dia
+            strFilterDia = "dia='" + dropDia.SelectedValue + "'";
+
+            try
+            {
+                //Refresh Palestra
+                DataTable dt = ds.Tables["tbPalestra"];
+                DataRow[] dr = dt.Select(strFilterDia + " AND " + strFilterPeriodo);
+                dropPalestra.DataSource = dr.CopyToDataTable();
+                dropPalestra.DataTextField = "nome";
+                dropPalestra.DataBind();
+
+                //Refresh Materia
+                dt = ds.Tables["tbMateria"];
+                dr = dt.Select(strFilterDia + " AND " + strFilterPeriodo);
+                dropMateria.DataSource = dr.CopyToDataTable();
+                dropMateria.DataTextField = "nome";
+                dropMateria.DataBind();
+            }
+            catch
+            {
+                //não possui nenhum eveto para esse dia
+                dropPalestra.Items.Clear();
+                dropMateria.Items.Clear();
+            }
+
+            //Response.Write("<script lang=javasript>$('.selectpicker').selectpicker();</script>");
+        }
+
+
+
+        private void Carrega_Periodo()
+        {
+            sql = "SELECT cod FROM periodo ORDER BY ord DESC";
+
+            conversor.SelectCommand = new MySqlCommand(sql, conn);
+            try
+            {
+                ds.Tables["tbPeriodo"].Clear();
+            }
+            catch
+            {
+                sql = "";
+            }
+            conversor.Fill(ds, "tbPeriodo");
+        }
+
+        private void Carrega_Dia()
+        {
+            sql = "SELECT dia FROM dia ORDER BY ord DESC";
+
+            conversor.SelectCommand = new MySqlCommand(sql, conn);
+            try
+            {
+                ds.Tables["tbDia"].Clear();
+            }
+            catch{ }
+            
+            conversor.Fill(ds, "tbDia");
+        }
+
+        private void Carrega_Palestra()
+        {
+            sql = "SELECT * FROM palestra";
+
+            conversor.SelectCommand = new MySqlCommand(sql, conn);
+            try
+            {
+                ds.Tables["tbPalestra"].Clear();
+            }
+            catch { }
+            conversor.Fill(ds, "tbPalestra");
+        }
+
+        private void Carrega_Materia()
+        {
+            sql = "SELECT * FROM materia";
+
+            conversor.SelectCommand = new MySqlCommand(sql, conn);
+            try
+            {
+                ds.Tables["tbMateria"].Clear();
+            }
+            catch { }
+            conversor.Fill(ds, "tbMateria");
+        }
+    }
+}
