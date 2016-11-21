58051d636b81decd134397dd9ef478461e3c94bb
derrix060 <mario_surfing@hotmail.com>
2016-11-21 20:23:37 -0200

    Database

---
+++ ChamadaEventosFatec/ChamadaEventosFatec/App_Code/BancoDados.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//DataSet
+using System.Data;
+//Database
+using MySql.Data.MySqlClient;
+
+namespace ChamadaEventosFatec.App_Code
+{
+    public class BancoDados
+    {
+        private static string sc = "Server=localhost;Database=chamada;uid=visual_studio;pwd=visual;";
+        private static MySqlConnection conn = new MySqlConnection(sc);
+        private static MySqlCommand cmd = new MySqlCommand();
+
+        public static void ExecuteSql(string sql)
+        {
+            cmd = new MySqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (MySqlException exe)
+            {
+                conn.Close();
+                throw new Exception("MySqlException: " + exe.Message);
+            }
+            catch (Exception exe)
+            {
+                conn.Close();
+                throw new Exception(exe.Message);
+            }
+        }
+
+        public static DataTable GetFromDB(string sql)
+        {
+            DataSet ds = new DataSet();
+            UpdateDataSet(ds, sql, "tbl", true);
+            return ds.Tables["tbl"];
+        }
+
+        public static void UpdateDataSet(DataSet ds, string sql, string tbName, bool removeOld)
+        {
+            cmd = new MySqlCommand(sql, conn);
+
+            try
+            {
+                if (removeOld)
+                    ds.Tables[tbName].Clear();
+            }
+            catch { }
+
+            try
+            {
+                cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter conversor = new MySqlDataAdapter();
+                conversor.SelectCommand = cmd;
+                conversor.Fill(ds, tbName);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.Message);
+            }
+        }
+
+    }
+}
---
+++ ChamadaEventosFatec/ChamadaEventosFatec/App_Code/Encrypt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+//MD5
+using System.Security.Cryptography;
+using System.Text; //UTF-8
+
+namespace ChamadaEventosFatec.App_Code
+{
+    public class Encrypt
+    {
+        static UTF8Encoding encoder = new UTF8Encoding();
+
+        public static Byte[] Criptografar(string campo)
+        {
+            // create the MD5CryptoServiceProvider object we will use to encrypt the password
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+
+            //create an array of bytes we will use to store the encrypted password
+            Byte[] resp;
+
+            //encrypt the password and store it in the hashedBytes byte array
+            resp = md5Hasher.ComputeHash(encoder.GetBytes(campo));
+
+            return resp;
+        }
+
+        public static Boolean Equals(Byte[] pri, Byte[] sec)
+        {
+            for (int i = 0; i < pri.Length; i++)
+            {
+                if (pri[i] != sec[i]) return false;
+            }
+            return true;
+        }
+
+        public static string CriptToString(Byte[] cript)
+        {
+            return encoder.GetString(cript);
+        }
+
+    }
+}
